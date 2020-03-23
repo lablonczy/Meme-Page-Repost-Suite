9336393cfbc11fc2055cfc251829f9171fa0292a
lablonczy <lukeablonczy@hotmail.com>
2020-03-23 16:01:21 -0400

    start

---
+++ InstaGrab.java
@@ -0,0 +1,23 @@
+package reposter;
+
+
+public class InstaGrab {
+	private String pageLink;
+	private String[] imageDirectLinks;
+
+	public InstaGrab(){
+		pageLink = null;
+		imageDirectLinks = null;
+	}
+
+	public InstaGrab(String link){
+
+	}
+
+	public String[] getPostContents(String link){
+		return null;
+	}
+
+
+
+}
